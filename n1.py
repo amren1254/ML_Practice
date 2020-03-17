e2207efa6d41162ab209d4a3aaff205b9e07bf2a
Amrendra <amren1254@gmail.com>
2020-03-17 20:58:24 +0530

    Script to check numpy version and configuration

---
+++ n1.py
@@ -0,0 +1,8 @@
+#1.Write a NumPy program to get the numpy version and show numpy build configuration. Go to the editor 
+
+#import numpy as np
+#print(np.__version__)
+#print(np.show_config())
+import numpy as np
+print(np.__version__)
+print(np.show_config())
